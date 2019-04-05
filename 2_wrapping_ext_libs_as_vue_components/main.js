28f636774f4e332965def69b04df912750328909
Melvin Wilson <melvinwilson2012@gmail.com>
2019-04-05 12:30:53 +1000

    Pickaday date now bound to the state

--- 2_wrapping_ext_libs_as_vue_components/main.js
+++ 2_wrapping_ext_libs_as_vue_components/main.js
@@ -2,13 +2,15 @@ new Vue({
     el: '#vueId',
 
     data: {
-        date:'5-04-2019',
+        date:'2019-05-04',
     },
 
     mounted: function() {
         var picker = new Pikaday({
             field: document.getElementById('datepicker'),
-            format: 'YYYY-MM-DD'
+            onSelect: () => {
+                this.date = picker.toString()
+            }
         })
     }
 
