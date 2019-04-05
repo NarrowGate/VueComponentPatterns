549ac2ffcdf1742ebc57c9e034a01f0aa9c437bf
Melvin Wilson <melvinwilson2012@gmail.com>
2019-04-05 13:53:19 +1000

    Formats through props
    Specifiy formats for pickaday from the consumer to the component

--- 2_wrapping_ext_libs_as_vue_components/components/pickaday.js
+++ 2_wrapping_ext_libs_as_vue_components/components/pickaday.js
@@ -1,11 +1,20 @@
 Vue.component('pickaday', {
     template: `<input :value="value" class="form-input" type="text" ref="someDatepicker">`,
 
-    props: ['value'],
+    props: {
+        value : {
+            required: true
+        },
+        format: {
+            default: 'YYYY-MM-DD'
+        }
+
+    },
 
     mounted: function() {
         var picker = new Pikaday({
             field: this.$refs.someDatepicker,
+            format: this.format,
             onSelect: () => {
                 this.$emit('input', picker.toString())
             }
